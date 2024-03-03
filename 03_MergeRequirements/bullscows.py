60b69964fee36624299d09ee6b2b8401dbf2db8b
Roman Danilov <romanosauce.code@gmail.com>
2024-03-03 18:31:02 +0300

    Add driver for main logic

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,6 +1,7 @@
 import collections
 import random
 import sys
+import urllib.request
 
 
 word_len = 5
@@ -21,17 +22,48 @@ def bullscows(guess: str, secret: str) -> (int, int):
 
     return (bulls, cows)
 
+
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
     valid_words = list(filter(lambda x: len(x) == word_len, words))
     if not valid_words:
         print("Can't find suitable words to choose")
         sys.exit(1)
     secret_word = random.choice(valid_words)
+    print("Secret:", secret_word)
     guess = ""
-    attemps = 0
+    attempts = 0
     while guess != secret_word:
         guess = ask("Введите слово: ", words)
         inform("Быки: {}, Коровы: {}", *bullscows(guess, secret_word))
-        attemps += 1
+        attempts += 1
 
-    return attemps
+    return attempts
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    print(prompt)
+    guess = input()
+    if valid:
+        while guess not in valid:
+            print("Invalid guess, try again")
+            guess = input()
+    return guess
+
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        word_len = int(sys.argv[2])
+    if len(sys.argv) not in [2, 3]:
+        print("usage: python -m bullscows <dict_path> <word_len>")
+    dict_path = sys.argv[1]
+    try:
+        with urllib.request.urlopen(dict_path) as f:
+            word_list = f.read().decode().split()
+    except Exception:
+        with open(dict_path) as f:
+            word_list = f.read().split()
+    print("Total attempts:", gameplay(ask, inform, word_list))
