924d46bb31ab72287cc6986b234dfd67bd780050
Roman Danilov <romanosauce.code@gmail.com>
2024-03-06 22:54:20 +0300

    Add help docstring for CLI commands

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -7,19 +7,50 @@ import readline
 class CowsayCmd(cmd.Cmd):
     prompt = "cowsay>> "
     intro = """
-Welcome to the cowsay CLI. It is fun, I promise
-"""
+        Welcome to the cowsay CLI. It is fun, I promise
+        """
 
     def do_list_cows(self, arg):
+        """
+        List all available cows in given directory (or default)
+
+        :param cow_path=PATH/TO/FILE
+        """
+
         print(cowsay.list_cows())
 
-    def make_bubble(self, arg):
+    def do_make_bubble(self, arg):
+        """
+        Wrap text in bubble
+
+        :param text=TEXT
+        :param width=WIDTH
+        """
+
         print(cowsay.make_bubble(arg))
 
-    def cowsay(self, arg):
+    def do_cowsay(self, arg):
+        """
+        Make cow say the text
+
+        :param text=TEXT
+        :param cow=COW_NAME
+        :param eyes=EYE_STRING
+        :param tongue=TONGUE_STRING
+        """
+
         print(cowsay.cowsay(arg))
 
-    def cowthink(self, arg):
+    def do_cowthink(self, arg):
+        """
+        Make cow say the text
+
+        :param text=TEXT
+        :param cow=COW_NAME
+        :param eyes=EYE_STRING
+        :param tongue=TONGUE_STRING
+        """
+
         print(cowsay.cowthink(arg))
 
 
