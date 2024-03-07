fc4aa143e4c822bde4ccb9807b1c9a0237267ff5
Roman Danilov <romanosauce.code@gmail.com>
2024-03-07 16:13:52 +0300

    Add option parsing for all commands

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -2,6 +2,7 @@ import cmd
 import cowsay
 import shlex
 import readline
+import sys
 
 
 class CowsayCmd(cmd.Cmd):
@@ -10,48 +11,90 @@ class CowsayCmd(cmd.Cmd):
         Welcome to the cowsay CLI. It is fun, I promise
         """
 
+    def get_option_value(self, opt, arg, cmd_name, val_type=str):
+        if opt in arg:
+            idx = arg.index(opt)
+            try:
+                val = val_type(arg[idx+1])
+            except Exception:
+                print("Wrong usage!")
+                self.do_help(cmd_name)
+                raise
+            return val
+        else:
+            return None
+
     def do_list_cows(self, arg):
         """
         List all available cows in given directory (or default)
 
-        :param cow_path=PATH/TO/FILE
+        :param -f PATH/TO/FILE
         """
 
-        print(cowsay.list_cows())
+        arg = shlex.split(arg)
+        try:
+            val_path = self.get_option_value('-f', arg, 'list_cows')
+        except Exception:
+            return
+        cow_path = val_path if val_path else cowsay.COW_PEN
+
+        print(cowsay.list_cows(cow_path=cow_path))
 
     def do_make_bubble(self, arg):
         """
         Wrap text in bubble
 
-        :param text=TEXT
-        :param width=WIDTH
+        :param -t TEXT
+        :param -w WIDTH
         """
 
-        print(cowsay.make_bubble(arg))
+        arg = shlex.split(arg)
+        try:
+            val_width = self.get_option_value('-w', arg, 'make_bubble', int)
+            val_text = self.get_option_value('-t', arg, 'make_bubble')
+        except Exception:
+            return
+        width = val_width if val_width else 40
+        text = val_text if val_text else sys.stdin.read()
 
-    def do_cowsay(self, arg):
+        print(cowsay.make_bubble(text=text, width=width))
+
+    def do_cowsay(self, arg, func=cowsay.cowsay):
         """
         Make cow say the text
 
-        :param text=TEXT
-        :param cow=COW_NAME
-        :param eyes=EYE_STRING
-        :param tongue=TONGUE_STRING
+        :param -t TEXT
+        :param -c COW_NAME
+        :param -e EYE_STRING
+        :param -T TONGUE_STRING
         """
 
-        print(cowsay.cowsay(arg))
+        arg = shlex.split(arg)
+        try:
+            val_text = self.get_option_value('-t', arg, 'cowsay')
+            val_eyes = self.get_option_value('-e', arg, 'cowsay')
+            val_tongue = self.get_option_value('-T', arg, 'cowsay')
+            val_cow = self.get_option_value('-c', arg, 'cowsay')
+        except Exception:
+            return
+        text = val_text if val_text else sys.stdin.read()
+        eyes = val_eyes if val_eyes else cowsay.Option.eyes
+        tongue = val_tongue if val_tongue else cowsay.Option.tongue
+        cow = val_cow if val_cow else 'default'
+
+        print(func(text, cow=cow, eyes=eyes, tongue=tongue))
 
     def do_cowthink(self, arg):
         """
-        Make cow say the text
+        Make cow think the text
 
-        :param text=TEXT
-        :param cow=COW_NAME
-        :param eyes=EYE_STRING
-        :param tongue=TONGUE_STRING
+        :param -t TEXT
+        :param -c COW_NAME
+        :param -e EYE_STRING
+        :param -T TONGUE_STRING
         """
 
-        print(cowsay.cowthink(arg))
+        self.do_cowsay(arg, func=cowsay.cowthink)
 
 
 if __name__ == '__main__':
