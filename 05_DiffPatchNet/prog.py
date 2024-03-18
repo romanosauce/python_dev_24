dbe66450079149495ff49b864550dd4c5c8a13c2
Roman Danilov <romanosauce.code@gmail.com>
2024-03-18 22:22:41 +0300

    Add working base for server

--- 05_DiffPatchNet/prog.py
+++ 05_DiffPatchNet/prog.py
@@ -1,34 +1,88 @@
 import cowsay
 import asyncio
 
-clients = {}
+clients = {}                        # cow name to client_id
+clients_queue = {}                  # client_id to client_queue
+clients_cows = {}                   # client_id to cow
 
 
 async def chat(reader, writer):
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    clients[me] = asyncio.Queue()
-    send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    client_id = "{}:{}".format(*writer.get_extra_info('peername'))
+    clients_queue[client_id] = asyncio.Queue()
+    receive_data_from_client = asyncio.create_task(reader.readline())
+    write_data_to_client = asyncio.create_task(clients_queue[client_id].get())
+    print(f"New connection from {client_id}")
+    writer.write('>>> '.encode())
+    await writer.drain()
+
     while not reader.at_eof():
-        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        done, pending = await asyncio.wait([receive_data_from_client, write_data_to_client],
+                                           return_when=asyncio.FIRST_COMPLETED)
         for q in done:
-            if q is send:
-                send = asyncio.create_task(reader.readline())
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
-            elif q is receive:
-                receive = asyncio.create_task(clients[me].get())
-                writer.write(f"{q.result()}\n".encode())
-                await writer.drain()
-    send.cancel()
-    receive.cancel()
-    print(me, "DONE")
-    del clients[me]
+            if q is receive_data_from_client:
+                receive_data_from_client = asyncio.create_task(reader.readline())
+                data = q.result().decode().strip()
+                data = data.split()
+                match data:
+                    case ['who']:
+                        writer.write((' '.join(clients.keys()) + '\n>>> ').encode())
+                    case ['cows']:
+                        writer.write((' '.join(cow for cow in cowsay.list_cows()
+                                               if cow not in clients) + '\n>>> ').encode())
+                    case ['login', cow]:
+                        if cow not in clients and cow in cowsay.list_cows():
+                            clients[cow] = client_id
+                            clients_cows[client_id] = cow
+                            writer.write('>>> '.encode())
+                        else:
+                            writer.write('invalid cow\n>>> '.encode())
+                    case ['say', cow, *text]:
+                        if client_id in clients_cows:
+                            if cow in clients:
+                                if text:
+                                    msg = f"\n{clients_cows[client_id]} said to you:\n{' '.join(text)}"
+                                    await clients_queue[clients[cow]].put(msg)
+                                    writer.write('>>> '.encode())
+                                else:
+                                    writer.write('invalid message\n>>> '.encode())
+                            else:
+                                writer.write('invalid cow\n>>> '.encode())
+                        else:
+                            writer.write("you can't send messages\nPlease log in\n>>> ".encode())
+                    case ['yield', *text]:
+                        if client_id in clients_cows:
+                            for queue in clients_queue.values():
+                                if queue is not clients_queue[client_id]:
+                                    msg = f"\n{clients_cows[client_id]} said to everyone:\n{' '.join(text)}"
+                                    await queue.put(msg)
+                                    writer.write('>>> '.encode())
+                        else:
+                            writer.write("you can't send messages\nPlease log in\n>>> ".encode())
+                    case ['quit']:
+                        print(f"We lost {client_id}")
+                        receive_data_from_client.cancel()
+                        write_data_to_client.cancel()
+                        del clients_queue[client_id]
+                        client_cow = clients_cows[client_id]
+                        del clients_cows[client_id]
+                        del clients[client_cow]
+                        writer.close()
+                        await writer.wait_closed()
+                        return
+            elif q is write_data_to_client:
+                write_data_to_client = asyncio.create_task(clients_queue[client_id].get())
+                data = q.result()
+                writer.write((data + '\n>>> ').encode())
+            await writer.drain()
+    print(f"We lost {client_id}")
+    receive_data_from_client.cancel()
+    write_data_to_client.cancel()
+    del clients_queue[client_id]
+    client_cow = clients_cows[client_id]
+    del clients_cows[client_id]
+    del clients[client_cow]
     writer.close()
     await writer.wait_closed()
-
 
 async def main():
     server = await asyncio.start_server(chat, '0.0.0.0', 1337)
